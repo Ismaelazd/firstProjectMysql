c22f93b9535014831e97c59d3f4eb85089c1b59d
Ismael <ismaelazd@hotmail.com>
2020-02-14 14:26:34 +0100

    first commit Mysql

---
+++ database/migrations/2020_02_14_130955_create_personnes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonnesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personnes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // $table->timestamps();
+            $table->char('nom',30);
+            $table->integer('age');
+            $table->timestamp('dateofbirth');
+            $table->unique('email');
+            $table->smallInteger('phonenumber');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personnes');
+    }
+}
